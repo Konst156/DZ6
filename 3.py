d30546a7b8941fd216138c2118b2659eecbf1501
Konstantin <kzhestovskih2012@gmail.com>
2022-12-15 18:00:13 +0500

    finish

--- 3.py
+++ 3.py
@@ -12,6 +12,13 @@ users = ['user1','user2','user3','user4','user5']
 ids = [4, 5, 9, 14, 7]
 salary = [111,222,333]
 
-a = zip(users, ids, salary)
-for i in a:
-    print(list(i))
+# a = zip(users, ids, salary)
+# print(a)
+# for i in a:
+#     print(list(i))
+
+a, b, c = map(list, zip(users, ids, salary))
+print(a, b, c, sep='\n')
+
+a, b, c = map(list, zip(a, b, c))
+print(a, b, c, sep='\n')
